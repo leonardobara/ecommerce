e8f3b8264627796e1c5d4f25b97d55513b77828e
Leonardo <leonardo.barahona96@outlook.com>
2021-08-20 20:51:48 -0600

    Adding security project: Core identity and DbContext configuration

---
+++ Microservices/Services.Security.Api/Core/Persistence/SecurityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Services.Security.Api.Core.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Security.Api.Core.Persistence
+{
+    public class SecurityContext: IdentityDbContext<User>
+    {
+        public SecurityContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
